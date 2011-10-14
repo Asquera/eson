d29a015b517d09492ded80081c262a5588a91702
Florian Gilcher <florian.gilcher@asquera.de>
2011-10-14 21:07:25 +0200

    Fix count API

--- lib/elasticsearch/http/core/count.rb
+++ lib/elasticsearch/http/core/count.rb
@@ -1,10 +1,21 @@
 module ElasticSearch
   module HTTP
     module Count
-      include Search
+      include Shared::Search
+      extend API
+      
+      request_method :post
+      
+      def bare_path
+        unless types.empty?
+          path = "{-list|,|indices}/{-list|,|types}/"
+        else
+          path = "{-list|,|indices}/"
+        end
+      end
       
       def path
-        bare_path + "_count"
+        path = bare_path + "_count"
       end
     end
   end
