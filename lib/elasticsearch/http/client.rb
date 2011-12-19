4c4a701e2c1da685eb886c18e5d3ac4d9934a3f3
Florian Gilcher <florian.gilcher@asquera.de>
2011-12-19 06:40:02 +0100

    Fix some search bugs

--- lib/elasticsearch/http/client.rb
+++ lib/elasticsearch/http/client.rb
@@ -4,11 +4,11 @@ module ElasticSearch
   module HTTP
     class Client < ElasticSearch::Client
       DEFAULT_OPTS = {
-                       :server => "http://localhost:9200", 
-                       :protocol => ElasticSearch::HTTP, 
+                       :server => "http://localhost:9200",
+                       :protocol => ElasticSearch::HTTP,
                        :plugins => [ElasticSearch::QueryPlugin, ElasticSearch::StatusHandler, ElasticSearch::ResponseParser]
                      }
-                     
+
       def initialize(opts = {})
         opts = DEFAULT_OPTS.merge(opts)
         super(opts)
