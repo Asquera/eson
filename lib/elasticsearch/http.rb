e0db2e5953ee359a1933a51dad257971db7b9b2c
Florian Gilcher <florian.gilcher@asquera.de>
2012-01-12 20:26:50 +0100

    Only load logger if necessary

--- lib/elasticsearch/http.rb
+++ lib/elasticsearch/http.rb
@@ -1,15 +1,14 @@
-require 'logger'
-
 module ElasticSearch
   module HTTP
     def self.logger=(logger)
       if String === logger
+        require 'logger'
         @logger = Logger.new(logger)
       else
         @logger = logger
       end
     end
-    
+
     def self.logger
       @logger
     end
