68ab1ef12d8ac720aabfebca9f8f3bcfd1146014
Florian Gilcher <florian.gilcher@asquera.de>
2012-03-11 11:43:14 +0100

    span_near implementation

--- lib/elasticsearch/search/span_near.rb
+++ lib/elasticsearch/search/span_near.rb
@@ -4,6 +4,25 @@ module ElasticSearch
       include Query
 
       short_name :span_near
+
+      attr_accessor :options
+
+      def initialize(options = {})
+        self.options = options
+      end
+
+      def clauses(&block)
+        @clauses ||= SubQuery.new
+        @clauses.send(context, &block)
+      end
+
+      def to_query_hash
+        result = {}
+
+        result[:clauses] = @clauses.to_query_hash
+
+        {name => result.merge(options)}
+      end
     end
   end
 end
--- test/search/query_test.rb
+++ test/search/query_test.rb
@@ -494,6 +494,24 @@ context "Queries" do
       end
     end
   end
+  
+  context "span_near query" do
+    query_name "test/search/queries/span_near"
+
+    setup do
+      ElasticSearch::Search::BaseQuery.new do
+        query do
+          span_near :slop => 12, :in_order => false, :collect_payloads => false do
+            clauses do
+              span_term :field => "value1"
+              span_term :field => "value2"
+              span_term :field => "value3"
+            end
+          end
+        end
+      end
+    end
+  end
 
   context "span_first query" do
     query_name "test/search/queries/span_first"
