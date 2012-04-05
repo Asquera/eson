576ed7ede3726ecd6ec58e41ab1e7f6ed1575403
Florian Gilcher <florian.gilcher@asquera.de>
2012-04-05 22:38:38 +0200

    Use gateway.type properly

--- test/test_config.rb
+++ test/test_config.rb
@@ -18,7 +18,7 @@ module Node
   module External
     def self.instance
       @node ||= begin 
-        node = ElasticSearch::Node::External.new("gateway" => "none")
+        node = ElasticSearch::Node::External.new("gateway.type" => "none")
         at_exit do
           node.close
         end
