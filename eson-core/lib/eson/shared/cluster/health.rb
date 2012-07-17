4a3185e66d0d645b1b876ed740340e3bfbadf264
Florian Gilcher <florian.gilcher@asquera.de>
2012-07-17 07:28:32 +0200

    Add docs to the health api

--- eson-core/lib/eson/shared/cluster/health.rb
+++ eson-core/lib/eson/shared/cluster/health.rb
@@ -1,10 +1,18 @@
 module Eson
   module Shared
+    # Requests using this API have the following properties:
+    #
+    # {include:Health#parameters}
+    # {include:Health#source_param}
+    # {include:Health#multi_index}
+    # {include:Health#multi_types}
     module Health
       extend API
       
+      # @!macro multi_index
       multi_index true
 
+      # @!macro parameters
       parameters :wait_for_status,
                  :level,
                  :wait_for_relocating_shards,
