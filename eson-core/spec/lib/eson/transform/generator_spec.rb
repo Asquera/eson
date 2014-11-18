b925bdf8dd266f7e8dbacaa831e0f40d10c27b99
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2014-11-18 18:02:12 +0100

    Checks parameter properties of test class

--- eson-core/spec/lib/eson/transform/generator_spec.rb
+++ eson-core/spec/lib/eson/transform/generator_spec.rb
@@ -6,8 +6,10 @@ require 'lib/eson/transform/generator'
 
 describe Eson::Transform::Generator do
   describe '#initialize' do
-    it 'does not raise error' do
-      expect { Eson::Transform::Generator.new({}) }.to_not raise_error
+    context 'with empty hash' do
+      it 'does not raise error' do
+        expect { Eson::Transform::Generator.new({}) }.to_not raise_error
+      end
     end
 
     context 'with sample "cluster.health"' do
@@ -24,11 +26,29 @@ describe Eson::Transform::Generator do
     context 'with sample "cluster.health"' do
       let(:sample) { load_api_sample('cluster.health') }
       let(:source) { Eson::Transform::Generator.new(sample).ruby_content }
-      subject!(:module) { eval(source) }
+      let!(:module) { eval(source) }
 
       it 'creates module Eson::Shared::Cluster::Health' do
         exists = Object.const_defined?('Eson::Shared::Cluster::Health')
         expect(exists).to eq true
+      end
+
+      context 'with concrete class' do
+        before do
+          class ApiTest
+            include Eson::Shared::Cluster::Health
+          end
+        end
+        subject(:api) { ApiTest.new }
+
+        it { is_expected.to respond_to(:level) }
+        it { is_expected.to respond_to(:local) }
+        it { is_expected.to respond_to(:master_timeout) }
+        it { is_expected.to respond_to(:timeout) }
+        it { is_expected.to respond_to(:wait_for_active_shards) }
+        it { is_expected.to respond_to(:wait_for_nodes) }
+        it { is_expected.to respond_to(:wait_for_relocating_shards) }
+        it { is_expected.to respond_to(:wait_for_status) }
       end
     end
   end
