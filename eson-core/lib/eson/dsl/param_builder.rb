bf7b4fe4275a3296d0fad1873f41757bef51a02e
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2014-11-21 20:02:42 +0100

    Refactors and extracts common set up to private method

--- eson-core/lib/eson/dsl/param_builder.rb
+++ eson-core/lib/eson/dsl/param_builder.rb
@@ -9,8 +9,7 @@ module Eson
         end
 
         def enum(name, enum_values = [], default = nil)
-          mod = Module.new do
-            include Virtus.module
+          in_module do
             attribute name.to_sym, String, default: default, coercer: proc { |value|
               unless enum_values.include?(value) || value.nil?
                 raise ArgumentError
@@ -18,30 +17,34 @@ module Eson
               value
             }
           end
-          extend mod
         end
 
         def string(name, default = nil)
-          mod = Module.new do
-            include Virtus.module
+          in_module do
             attribute name.to_sym, String, default: default
           end
-          extend mod
         end
 
         def boolean(name, default = nil)
-          mod = Module.new do
-            include Virtus.module
+          in_module do
             attribute name.to_sym, 'Boolean', default: default
           end
-          extend mod
         end
 
         def numnber(name, default = nil)
-          mod = Module.new do
-            include Virtus.module
+          in_module do
             attribute name.to_sym, Fixnum, default: default
           end
+        end
+
+        private
+
+        def in_module(&block)
+          mod = Module.new do
+            include Virtus.module
+            instance_eval(&block) if block_given?
+          end
+          extend mod
         end
       end
     end
