cc25f2c2f514442c03b1723bbc7af5eb41afc639
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2014-11-22 01:12:08 +0100

    Adds a more complete example with tests

--- eson-core/spec/lib/eson/api_spec.rb
+++ eson-core/spec/lib/eson/api_spec.rb
@@ -41,4 +41,29 @@ describe 'Eson::Api' do
       expect(subject.url.params).to respond_to :bar
     end
   end
+
+  describe 'more complete example' do
+    subject do
+      M.class_eval do
+        url do
+          set_base_path '/_cluster/health'
+          path '/_cluster/health'
+          path '/_cluster/health/{index}'
+
+          part :index, type: String, required: true
+        end
+      end
+      C.new
+    end
+
+    it 'does not raise error' do
+      expect { subject }.to_not raise_error
+    end
+
+    it { is_expected.to respond_to(:url) }
+
+    it 'contains correct base_path' do
+      expect(subject.url.base_path).to eq '/_cluster/health'
+    end
+  end
 end
