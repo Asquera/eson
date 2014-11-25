e01f2980891358f2438260b6bb921cb2561dec06
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2014-11-25 11:47:41 +0100

    Extends generator CLI tool to transform a folder with JSON specs to ruby files
    * a second CLI command transforms the JSON spec files from a folder to an output folder

--- eson-core/bin/generator.rb
+++ eson-core/bin/generator.rb
@@ -1,13 +1,29 @@
+require 'fileutils'
 require 'thor'
 require 'eson-core'
 
-class GeneratorCLI < Thor
-  desc "generate", "Generates a Ruby class file from a API description in JSON"
-  option :input,  aliases: '-i', type: :string, required: true, desc: "Specifies the input JSON file"
-  option :output, aliases: '-o', type: :string, retuired: true, desc: "Specifies the generated output ruby file"
-  def generate
-    input_name = options[:input]
-    hash = JSON.parse(File.read(input_name))
+module Helpers
+  def ruby_file_name(filename)
+    File.basename(filename, File.extname(filename)) + ".rb"
+  end
+
+  def folder_name(filename)
+    names = filename.split('.')
+    names = names.shift(names.size - 2)
+    File.join(names)
+  end
+
+  def short_name(filename)
+    names = filename.split('.')
+    names.shift(names.size - 2)
+    names.first
+  end
+
+  def transform(filename)
+    unless File.exists?(filename)
+      raise "File #{filename} does not exists"
+    end
+    hash = JSON.parse(File.read(filename))
     name, root = hash.first
     args = {
       name:            name,
@@ -16,9 +32,62 @@ class GeneratorCLI < Thor
       body:            root.fetch('body'),
       documentation:   root.fetch('documentation')
     }
-    generated = Eson::Transform::Generator.new(args).ruby_content
-    output_file = options[:output] || File.basename(input_name, File.extname(input_name)) + ".rb"
+    Eson::Transform::Generator.new(args).ruby_content
+  end
+end
+
+class GeneratorCLI < Thor
+  include Helpers
+
+  desc "transform file", "Generates a Ruby class file from a API description in JSON"
+  option :input,  aliases: '-i', type: :string, required: true, desc: "Specifies the input JSON file"
+  option :output, aliases: '-o', type: :string, retuired: true, desc: "Specifies the generated output ruby file"
+  def transform_file
+    generated   = transform(options[:input])
+    output_file = options[:output] || ruby_file_name(input_name)
     File.open(output_file, 'w') { |f| f.write(generated) }
+  end
+
+  desc "generate", "Takes JSON files from a folder and generate ruby files into another folder"
+  option :input_folder,  aliases: '-i', type: :string, required: true, desc: "Specifies the input folder with JSON files"
+  option :output_folder, aliases: '-o', type: :string, required: true, desc: "Specifies the output folder for all generated ruby files"
+  option :overwrite,                    type: :boolean, required: false, desc: "If set it overwrites the content of the output folder"
+  def generate
+    json_files = Dir.glob(File.join(options[:input_folder], '**/*.json'))
+    output_folder = File.absolute_path(options.fetch('output_folder'))
+
+    # if folder exists warn
+    if Dir.exists?(output_folder)
+      if options['overwrite']
+        # Dir.rmdir(output_folder)
+        FileUtils.rm_rf(output_folder)
+      else
+        raise ArgumentError, "Folder #{output_folder} exists"
+      end
+    end
+
+    Dir.mkdir(output_folder)
+
+    # generate all ruby contents for all files put into a map
+    # create folders / files
+    result = {}
+    json_files.each do |file|
+      STDOUT.puts "Transforming file #{file}"
+      result[ruby_file_name(file)] = transform(file)
+    end
+
+    # write folders / files
+    result.each do |file, content|
+      folder = File.join(output_folder, folder_name(file))
+      output_file = File.join(folder, short_name(file)) + ".rb"
+
+      unless Dir.exists?(folder) || folder.empty?
+        Dir.mkdir(folder)
+      end
+
+      STDOUT.puts "writing file: #{output_file}"
+      File.open(output_file, "w") { |f| f.write(content) }
+    end
   end
 end
 
