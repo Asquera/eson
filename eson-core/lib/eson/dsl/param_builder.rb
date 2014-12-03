f7d4a56d0e03728a74afb1e370183727abdffc7a
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2014-12-03 14:07:50 +0100

    Refactors creation of Module in ParamBuilder class

--- eson-core/lib/eson/dsl/param_builder.rb
+++ eson-core/lib/eson/dsl/param_builder.rb
@@ -5,67 +5,63 @@ module Eson
     module DSL
       class ParamBuilder
         def initialize(&block)
+          @mod = create_module
           instance_eval(&block) if block_given?
         end
 
         def enum(name, enum_values = [], default = nil)
-          in_module do
-            attribute name.to_sym, String, default: default, coercer: proc { |value|
-              values = Array(value)
-              unless values.all?{ |v| enum_values.include?(v) } || value.nil?
-                raise ArgumentError
-              end
-              value
-            }
-          end
+          coercer = proc { |value|
+            values = Array(value)
+            unless values.all?{ |v| enum_values.include?(v) } || value.nil?
+              raise ArgumentError
+            end
+            value
+          }
+          add_attribute name, String, default, coercer
         end
 
         def string(name, default = nil)
-          in_module do
-            attribute name.to_sym, String, default: default
-          end
+          add_attribute name, String, default
         end
 
         alias :text :string
 
         def list(name, default = nil)
-          in_module do
-            attribute name.to_sym, Array[String], default: default
-          end
+          add_attribute name, Array[String], default
         end
 
         def boolean(name, default = nil)
-          in_module do
-            attribute name.to_sym, 'Boolean', default: default
-          end
+          add_attribute name, 'Boolean', default
         end
 
         def number(name, default = nil)
-          in_module do
-            attribute name.to_sym, Fixnum, default: default
-          end
+          add_attribute name, Fixnum, default
         end
 
         def duration(name, default = nil)
-          in_module do
-            attribute name.to_sym, Integer, default: default
-          end
+          add_attribute name, Integer, default
         end
 
         def time(name, default = nil)
-          in_module do
-            attribute name.to_sym, DateTime, default: default
-          end
+          add_attribute name, DateTime, default
         end
 
         private
 
-        def in_module(&block)
-          @mod ||= Module.new do
+        def create_module
+          m = Module.new do
             include Virtus.module
           end
-          @mod.instance_eval(&block) if block_given?
-          extend @mod
+          self.extend m
+        end
+
+        def add_attribute(name, type, default = nil, coercer = nil)
+          args = {}
+          args[:coercer] = coercer if coercer
+          @mod.instance_eval do
+            attribute name.to_sym, type, args
+          end
+          send("#{name}=", default)
         end
       end
     end
